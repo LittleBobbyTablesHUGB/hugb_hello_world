be0665c696b2528ec5dfe80f8f8c05e6b281db66
Harpa Guðjónsdóttir <harpa09@i-1217c9a8.cs1004cloud.internal>
2016-10-14 14:35:26 +0000

    Step4: Introduce library usage

--- src/main/java/com/bangsapabbi/helloworld/World.java
+++ src/main/java/com/bangsapabbi/helloworld/World.java
@@ -1,10 +1,16 @@
 package com.bangsapabbi.helloworld;
+import net.joningi.icndb.ICNDBClient;
+import net.joningi.icndb.Joke;
 
 public class World {
 
-    private String greeting = "Hello world!";
-
+    //private String greeting = "Hello world!";
+    
+    //Initialize the client
+    final ICNDBClient client = new ICNDBClient();
+    //Get random joke
+    private Joke randomJoke = client.getRandom();
     public String greet() {
-        return greeting;
+        return randomJoke.getJoke();
     }
 }
